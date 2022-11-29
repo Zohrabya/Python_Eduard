79895ccd50299312cd5e771951870c9e9ec9164c
eduardisk <eduard@iskography.com>
2022-11-29 17:35:58 +0400

    exe_03_08 1st commit

---
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_08/exercise_03_08.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+total = num1 + num2 + num3
+average = total / 3
+print(f"The sum is {total}")
+print(f"The average is {average}")
