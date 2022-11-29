62914b6514c3a2d5f28380a163e7603ad898761a
eduardisk <eduard@iskography.com>
2022-11-29 17:38:43 +0400

    exe_03_10 1st commit

---
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_10/exercise_03_10.py
@@ -0,0 +1,4 @@
+room_width = float(input("Please enter the width of the room in meters: "))
+room_lenght = float(input("Please enter the lenght of the room in meters: "))
+room_area = room_lenght * room_width
+print(f"The area of the room is {room_area} meters")
