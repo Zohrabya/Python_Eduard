785aa3e9c193129cf40bb3cbff050723e26da64e
eduardisk <eduard@iskography.com>
2022-11-29 17:39:57 +0400

    exe_03_11 1st commit

---
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_11/exercise_03_11.py
@@ -0,0 +1,4 @@
+field_lenght = int(input("Please enter the lenght of the field in feet: "))
+field_width = int(input("Please enter the lenght of the field in feet: "))
+field_area = field_lenght * field_width
+print(f"The area of the field is {field_area / 43560}")
