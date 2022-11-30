3084fafe541d29c0777e1311ebabaf64d3a76c70
eduardisk <eduard@iskography.com>
2022-11-30 14:30:24 +0400

    exe_03_15 1st commit

---
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_15/exercise_03_15.py
@@ -0,0 +1,25 @@
+State the order of evaluation of the operators in each of the following Python statements and show the value of x after each statement is performed.
+
+a.	x = 7 + 3 * 6 / 2 - 1
+
+1. 6 / 2
+2. 3 * (6 / 2)
+3. 7 + (3 * (6 / 2))
+4. (7 + (3 * (6 / 2))) - 1
+x = 15
+
+b.	x = 2 % 2 + 2 * 2 - 2 / 2
+
+1. 2 % 2 
+2. (2 % 2) + (2 * 2)
+3. ((2 % 2) + (2 * 2) - (2 / 2))
+x = 3
+
+c.	x = ( 3 * 9 * ( 3 + ( 9 * 3 / ( 3 ) ) ) )
+
+1. 3 / (3)
+2. 9 * (3 / (3))
+3. 3 + (9 * (3 / (3)))
+4. 9 * (3 + (9 * (3 / (3))))
+5. 3 * (9 * (3 + (9 * (3 / (3)))))
+x = 324
