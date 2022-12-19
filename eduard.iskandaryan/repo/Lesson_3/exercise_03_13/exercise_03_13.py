9bbdc567f261204620f17ed39c3fa0f6dc604193
eduardisk <eduard@iskography.com>
2022-12-19 18:43:43 +0400

    exe_03_13 2nd commit

--- eduard.iskandaryan/repo/Lesson_3/exercise_03_13/exercise_03_13.py
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_13/exercise_03_13.py
@@ -1,5 +1,6 @@
 meal_cost = float(input("Please enter the cost of the meal: "))
-meal_tax = meal_cost * 10/100
-meal_tip = meal_cost * 18/100
+tax_percent = 18
+meal_tax = meal_cost * 10 / 100
+meal_tip = meal_cost * tax_percent / 100
 
 print(f"Your tax amount is {round(meal_tax, 2)}, tip amount is {round(meal_tip, 2)} and the grand total is {round(meal_cost + meal_tax + meal_tip, 2)}")
