82df3988e5f993615e339bc69672fb708b9dff52
eduardisk <eduard@iskography.com>
2022-12-19 18:21:05 +0400

    exe_03_07 2nd commit

--- eduard.iskandaryan/repo/Lesson_3/exercise_03_07/exercise_03_07.py
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_07/exercise_03_07.py
@@ -1,2 +1,2 @@
-x = int(input("Enter a weight in kilograms: "))
-print(f"It will be {x * 2.2} in pounds")
+weight = int(input("Enter a weight in kilograms: "))
+print(f"It will be {weight * 2.2} in pounds")
