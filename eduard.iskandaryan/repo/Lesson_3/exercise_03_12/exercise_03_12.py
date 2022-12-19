f9ecd0293a6caa601740e0ee3388b20e76fb6162
eduardisk <eduard@iskography.com>
2022-12-19 18:39:39 +0400

    exe_03_12 3rd commit

--- eduard.iskandaryan/repo/Lesson_3/exercise_03_12/exercise_03_12.py
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_12/exercise_03_12.py
@@ -2,4 +2,5 @@ less_one_liter = 0.10
 more_one_liter = 0.25
 large_cups = int(input("Please enter number of returned large cups: "))
 small_cups = int(input("Please enter number of returned small cups: "))
-print(f"You will be refunded ${float((less_one_liter * small_cups)) + (more_one_liter * large_cups)} from all cups.")
+refund = "{:.2f}".format((less_one_liter * small_cups) + (more_one_liter * large_cups))
+print(f"You will be refunded ${refund} from all cups.")
