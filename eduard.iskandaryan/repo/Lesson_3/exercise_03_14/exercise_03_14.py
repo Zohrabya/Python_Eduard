e8d2d347e7a0607fc74ffe95a59a0f82d536bb9d
eduardisk <eduard@iskography.com>
2023-01-10 13:19:18 +0400

    exe_03_14 2nd commit

--- eduard.iskandaryan/repo/Lesson_3/exercise_03_14/exercise_03_14.py
+++ eduard.iskandaryan/repo/Lesson_3/exercise_03_14/exercise_03_14.py
@@ -1,3 +1,5 @@
 Given the algebraic equation ax3 + 7, which of the following, if any, are correct Python statements for this equation?
 
+a.	y = a * x * x * x + 7
+d.	y = (a*x) * x * x + 7
 e.	y = a * (x * x * x) + 7
